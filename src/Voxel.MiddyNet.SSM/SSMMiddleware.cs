7f48df9ec2c161693a92189bc0c045dcbba2117e
Vicenc Garcia <vgarcia@voxelgroup.net>
2020-08-04 14:05:33 +0100

    Initial commit

---
+++ src/Voxel.MiddyNet.SSM/SSMMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+
+namespace Voxel.MiddyNet.SSM
+{
+    public class SSMMiddleware<Req, Res> : ILambdaMiddleware<Req, Res>
+    {
+        private string ParameterPath { get; }
+        public SSMMiddleware(string parameterPath)
+        {
+            ParameterPath = parameterPath;
+        }
+
+        public Task<Res> After(Res lambdaResponse, MiddyNetContext context)
+        {
+            return Task.FromResult(lambdaResponse);
+        }
+
+        public async Task Before(Req lambdaEvent, MiddyNetContext context)
+        {
+            using var ssmClient = new AmazonSimpleSystemsManagementClient();
+            var cloudTradeAuthResponse = await ssmClient.GetParameterAsync(new GetParameterRequest()
+            {
+                Name = ParameterPath
+            });
+
+            context.AdditionalContext.Add("SSSParameter", cloudTradeAuthResponse.Parameter.Value); // Need more work
+        }
+    }
+}
---
+++ src/Voxel.MiddyNet/ILambdaMiddleware.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Voxel.MiddyNet
+{
+    public interface ILambdaMiddleware<Req, Res>
+    {
+        Task Before(Req lambdaEvent, MiddyNetContext context);
+        Task<Res> After(Res lambdaResponse, MiddyNetContext context);
+    }
+}
