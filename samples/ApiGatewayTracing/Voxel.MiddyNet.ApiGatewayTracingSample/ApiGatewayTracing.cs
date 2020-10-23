fe0a3099c4b9f631a2f2d7a071ae2c1492270f6e
mherrera <mherrera@voxelgroup.net>
2020-10-23 07:59:12 -0300

    feat: new project to add sample ApiGatewayTracing

---
+++ samples/ApiGatewayTracing/Voxel.MiddyNet.ApiGatewayTracingSample/ApiGatewayTracing.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Voxel.MiddyNet.Tracing.ApiGatewayMiddleware;
+
+namespace Voxel.MiddyNet.ApiGatewayTracingSample
+{
+    public class ApiGatewayTracing : MiddyNet<APIGatewayProxyRequest, int>
+    {
+        public ApiGatewayTracing()
+        {
+            Use(new ApiGatewayTracingMiddleware<int>());
+        }
+
+        protected override Task<int> Handle(APIGatewayProxyRequest proxyRequest, MiddyNetContext context)
+        {
+            var originalTraceParentHeaderValue = string.Empty;
+            if (proxyRequest.Headers.ContainsKey("traceparent"))
+            {
+                originalTraceParentHeaderValue = proxyRequest.Headers["traceparent"];
+            }
+
+            context.Logger.Log(LogLevel.Info, "Function called", new LogProperty("original-traceparent", originalTraceParentHeaderValue));
+
+            return Task.FromResult(0);
+        }
+    }
+}
