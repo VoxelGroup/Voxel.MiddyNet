a8bbf7ba265ab34fedb41c76e8ff84ea18c3b4ca
mintxelas <fermin.saez@gmail.com>
2020-10-30 12:15:53 +0100

    feat: setup npm, add missing serialization attribute.

--- samples/ApiGatewayTracing/Voxel.MiddyNet.ApiGatewayTracingSample/ApiGatewayTracing.cs
+++ samples/ApiGatewayTracing/Voxel.MiddyNet.ApiGatewayTracingSample/ApiGatewayTracing.cs
@@ -1,17 +1,19 @@
 ﻿using System.Threading.Tasks;
 using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
 using Voxel.MiddyNet.Tracing.ApiGatewayMiddleware;
 
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace Voxel.MiddyNet.ApiGatewayTracingSample
 {
-    public class ApiGatewayTracing : MiddyNet<APIGatewayProxyRequest, int>
+    public class ApiGatewayTracing : MiddyNet<APIGatewayProxyRequest, APIGatewayProxyResponse>
     {
         public ApiGatewayTracing()
         {
-            Use(new ApiGatewayTracingMiddleware<int>());
+            Use(new ApiGatewayTracingMiddleware<APIGatewayProxyResponse>());
         }
 
-        protected override Task<int> Handle(APIGatewayProxyRequest proxyRequest, MiddyNetContext context)
+        protected override Task<APIGatewayProxyResponse> Handle(APIGatewayProxyRequest proxyRequest, MiddyNetContext context)
         {
             var originalTraceParentHeaderValue = string.Empty;
             if (proxyRequest.Headers.ContainsKey("traceparent"))
@@ -21,7 +23,12 @@ namespace Voxel.MiddyNet.ApiGatewayTracingSample
 
             context.Logger.Log(LogLevel.Info, "Function called", new LogProperty("original-traceparent", originalTraceParentHeaderValue));
 
-            return Task.FromResult(0);
+            return Task.FromResult(new APIGatewayProxyResponse()
+            {
+                StatusCode = 200,
+                Body = "Ok", 
+                IsBase64Encoded = false              
+            });
         }
     }
 }
