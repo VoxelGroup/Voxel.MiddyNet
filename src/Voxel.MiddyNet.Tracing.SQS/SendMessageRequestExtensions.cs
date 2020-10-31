7bec36e21645e858afc2fdd817f3056247359d78
josep-bocanegra <40354250+josep-bocanegra@users.noreply.github.com>
2020-10-31 22:31:49 +0100

    feat: enrich message attributes with trace context

--- src/Voxel.MiddyNet.Tracing.SQS/SendMessageRequestExtensions.cs
+++ src/Voxel.MiddyNet.Tracing.SQS/SendMessageRequestExtensions.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Amazon.SQS.Model;
+﻿using Amazon.SQS.Model;
 using Voxel.MiddyNet.Tracing.Core;
 
 namespace Voxel.MiddyNet.Tracing.SQS
 {
     public static class SendMessageRequestExtensions
     {
+        private const string TraceParentHeaderName = "traceparent";
+        private const string TraceStateHeaderName = "tracestate";
+
         public static void EnrichWithTraceContext(this SendMessageRequest sendMessageRequest, TraceContext traceContext)
         {
-           
+            sendMessageRequest.AddMessageAttribute(TraceParentHeaderName, traceContext.TraceParent);
+            sendMessageRequest.AddMessageAttribute(TraceStateHeaderName, traceContext.TraceState);
+        }
+
+        private static void AddMessageAttribute(this SendMessageRequest publishRequest, string key, string value)
+        {
+            publishRequest.MessageAttributes.Add(key, new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = value
+            });
         }
     }
 }
