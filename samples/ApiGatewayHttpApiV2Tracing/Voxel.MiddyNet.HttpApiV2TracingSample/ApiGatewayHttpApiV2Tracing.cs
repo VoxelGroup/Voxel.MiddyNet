0d5dd89d0c00a924b4056a1383b0e0e69d1e4198
josep-bocanegra <40354250+josep-bocanegra@users.noreply.github.com>
2021-01-08 17:39:45 +0100

    fix: correct namespace

--- samples/ApiGatewayHttpApiV2Tracing/Voxel.MiddyNet.HttpApiV2TracingSample/ApiGatewayHttpApiV2Tracing.cs
+++ samples/ApiGatewayHttpApiV2Tracing/Voxel.MiddyNet.HttpApiV2TracingSample/ApiGatewayHttpApiV2Tracing.cs
@@ -4,7 +4,7 @@ using Amazon.Lambda.Core;
 using Voxel.MiddyNet.Tracing.ApiGatewayMiddleware;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
-namespace Voxel.MiddyNet.HttpApiTracingSample
+namespace Voxel.MiddyNet.HttpApiV2TracingSample
 {
     public class ApiGatewayHttpApiV2Tracing : MiddyNet<APIGatewayHttpApiV2ProxyRequest, APIGatewayHttpApiV2ProxyResponse>
     {
