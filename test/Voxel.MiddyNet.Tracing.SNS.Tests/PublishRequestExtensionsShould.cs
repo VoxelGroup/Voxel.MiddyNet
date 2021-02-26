540dc909f0630e1b4a51a00dfbc3d2927acf7ffa
mherrera <mherrera@voxelgroup.net>
2021-02-26 05:57:33 -0300

    test: enrich message attributes with trace context

--- test/Voxel.MiddyNet.Tracing.SNS.Tests/PublishRequestExtensionsShould.cs
+++ test/Voxel.MiddyNet.Tracing.SNS.Tests/PublishRequestExtensionsShould.cs
@@ -1,4 +1,7 @@
 using System;
+using Amazon.SimpleNotificationService.Model;
+using FluentAssertions;
+using Voxel.MiddyNet.Tracing.Core;
 using Xunit;
 
 namespace Voxel.MiddyNet.Tracing.SNS.Tests
@@ -6,9 +9,16 @@ namespace Voxel.MiddyNet.Tracing.SNS.Tests
     public class PublishRequestExtensionsShould
     {
         [Fact]
-        public void Test1()
+        public void EnrichMessageAttributesWithTraceContext()
         {
+            var publishRequest = new PublishRequest();
+            var traceContext = TraceContext.Handle("00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01",
+                "congo=t61rcWkgMzE");
 
+            publishRequest.EnrichWithTraceContext(traceContext);
+
+            publishRequest.MessageAttributes.Should().ContainKey("traceparent");
+            publishRequest.MessageAttributes.Should().ContainKey("tracestate");
         }
     }
 }
