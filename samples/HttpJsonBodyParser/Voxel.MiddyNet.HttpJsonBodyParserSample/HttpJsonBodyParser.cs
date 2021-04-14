f4e13642e84abba8b7d78ae56152234db2942922
Xavi Belloso <xbelloso@voxelgroup.net>
2021-04-14 09:15:11 +0200

    chore: add HttpJsonBodyParser Middleware Sample

---
+++ samples/HttpJsonBodyParser/Voxel.MiddyNet.HttpJsonBodyParserSample/HttpJsonBodyParser.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using Voxel.MiddyNet.HttpJsonBodyParserMiddleware;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+namespace Voxel.MiddyNet.HttpJsonBodyParserSample
+{
+    public class HttpJsonBodyParser : MiddyNet<APIGatewayProxyRequest, APIGatewayProxyResponse>
+    {
+        public HttpJsonBodyParser()
+        {
+            Use(new HttpJsonBodyParserMiddleware<Person>());
+        }
+
+        protected override Task<APIGatewayProxyResponse> Handle(APIGatewayProxyRequest lambdaEvent, MiddyNetContext context)
+        {
+            var person = ((Person) context.AdditionalContext["Body"]);
+            context.Logger.Log(LogLevel.Info, "Function called", new LogProperty("body", person));
+            var result = new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Body = $"Person name is {person.Name} {person.Surname} and it is {person.Age} years old."
+            };
+
+            return Task.FromResult(result);
+        }
+    }
+}
