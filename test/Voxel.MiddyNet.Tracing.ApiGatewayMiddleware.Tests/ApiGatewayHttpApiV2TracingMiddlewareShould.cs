c775c02865f20cf78065f28a446c47f5609d08d1
josep-bocanegra <40354250+josep-bocanegra@users.noreply.github.com>
2021-04-16 15:55:21 +0200

    test: enrich log without mutating parentid

--- test/Voxel.MiddyNet.Tracing.ApiGatewayMiddleware.Tests/ApiGatewayHttpApiV2TracingMiddlewareShould.cs
+++ test/Voxel.MiddyNet.Tracing.ApiGatewayMiddleware.Tests/ApiGatewayHttpApiV2TracingMiddlewareShould.cs
@@ -9,27 +9,30 @@ namespace Voxel.MiddyNet.Tracing.ApiGatewayMiddleware.Tests
 {
     public class ApiGatewayHttpApiV2TracingMiddlewareShould
     {
+        private const string TraceparentHeaderValue = "00-0af7651916cd43dd8448eb211c80319c-b9c7c989f97918e1-01";
+        private const string TracestateHeaderValue = "congo=ucfJifl5GOE";
+        private const string TraceIdHeaderValue = "0af7651916cd43dd8448eb211c80319c";
+
         [Fact]
         public async Task EnrichLoggerWithTraceContext()
         {
             var logger = Substitute.For<IMiddyLogger>();
             var context = new MiddyNetContext(Substitute.For<ILambdaContext>(), _ => logger);
-
             var middleware = new ApiGatewayHttpApiV2TracingMiddleware();
-
-            var apiGatewayHttpApiV2Event = new APIGatewayHttpApiV2ProxyRequest
+            var apiGatewayEvent = new APIGatewayHttpApiV2ProxyRequest
             {
                 Headers = new Dictionary<string, string>
                 {
-                    { "traceparent", "traceparent header value" },
-                    {"tracestate", "tracestate header value" }
+                    { "traceparent", TraceparentHeaderValue },
+                    {"tracestate", TracestateHeaderValue }
                 }
             };
 
-            await middleware.Before(apiGatewayHttpApiV2Event, context);
-            logger.Received().EnrichWith(Arg.Is<LogProperty>(p => p.Key == "traceparent"));
-            logger.Received().EnrichWith(Arg.Is<LogProperty>(p => p.Key == "tracestate"));
-            logger.Received().EnrichWith(Arg.Is<LogProperty>(p => p.Key == "trace-id"));
+            await middleware.Before(apiGatewayEvent, context);
+
+            logger.Received().EnrichWith(Arg.Is<LogProperty>(p => p.Key == "traceparent" && p.Value.ToString() == TraceparentHeaderValue));
+            logger.Received().EnrichWith(Arg.Is<LogProperty>(p => p.Key == "tracestate" && p.Value.ToString() == TracestateHeaderValue));
+            logger.Received().EnrichWith(Arg.Is<LogProperty>(p => p.Key == "trace-id" && p.Value.ToString() == TraceIdHeaderValue));
         }
     }
 }
